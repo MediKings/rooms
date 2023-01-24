b796a1e2f06d0836102737f8c316fee8ab0d161b
Medi King's <jehovahmbote3@gmail.com>
2023-01-24 13:17:43 +0100

    Slider

--- static/js/main.js
+++ static/js/main.js
@@ -4,4 +4,23 @@ $(document).ready(()=>{
         $('#menu .nav').toggleClass('show hide')
     })
 
+
+    $('.sliders').slick({
+        dots: false,
+        autoplay: true,
+        infinite: true,
+        speed: 150,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        responsive: [
+           {
+              breakpoint: 768,
+              settings: {
+                 slidesToShow: 1,
+                 slidesToScroll: 1,
+              }
+           },
+        ]
+     })
+
 })
